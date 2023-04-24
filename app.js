eb43e7c29f94e12886dc58a7e7f88003efeb3e2f
muhammad0936 <131200116+muhammad0936@users.noreply.github.com>
2023-04-24 06:57:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb://localhost:27017/?directConnection=true';
+  'mongodb+srv://muhammadkh:muhamkh0936@cluster0.qno7rfp.mongodb.net/?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
